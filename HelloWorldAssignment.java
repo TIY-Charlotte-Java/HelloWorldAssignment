2389dda506dfc2a3c83d6cccd58eac40a55de901
Ben <bburrva@gmail.com>
2016-11-28 14:19:09 -0500

    corrected missing comments

--- HelloWorldAssignment.java
+++ HelloWorldAssignment.java
@@ -19,7 +19,9 @@ basic format for java.
 then:
 git add .
 git status - look for newly added java file
-git commit
-git status - should be 
+git commit -m "some sort of comment"
+git status - should be "nothing to commit. Working Branch Clean"
+git push - send the file to the fork on github
+
 
 */
