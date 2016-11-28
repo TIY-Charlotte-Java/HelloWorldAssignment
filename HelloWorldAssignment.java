5ba254dcd00071ed626e4925df23808e20a56901
Ben <bburrva@gmail.com>
2016-11-28 14:25:01 -0500

    added more notes on assignment process

--- HelloWorldAssignment.java
+++ HelloWorldAssignment.java
@@ -23,5 +23,13 @@ git commit -m "some sort of comment"
 git status - should be "nothing to commit. Working Branch Clean"
 git push - send the file to the fork on github
 
+next:
+go to github.com 
+your repositories
+current assignment fork
+check the files have be sent to github
+if yes, create pull request. 
+name pull request. unique to assignment. 
+copy url after submitting pull request to assignment page on online.theironyard.com
 
 */
