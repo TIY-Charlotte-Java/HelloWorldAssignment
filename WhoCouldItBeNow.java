0b2572981f56716d6371d07e66d9caa2b05ae1d6
GraceConnelly <grace.connelly.a@gmail.com>
2016-11-28 18:26:04 -0500

    Added my name as a variable and another possible name variable to really solve the age old question of - Who could it be now?

--- WhoCouldItBeNow.java
+++ WhoCouldItBeNow.java
@@ -1,6 +1,29 @@
 class WhoCouldItBeNow {
- public static void main(String[] args) {
-   System.out.println("It is Grace Connelly");
+
+// establishes a few names as variables and defines what to print
+
+
+  public static void main(String[] args) {
+    String aName = "Grace Connelly";
+    String bName = "Sylvia";
+    String cName = whoIsIt(aName, bName);
+
+    System.out.println("It is" + cName);
+  }
+
+
+
+
+//method for determining the longer name to know who it really is
+
+  public static String whoIsIt(String a,String b) {
+    String itIs;
+    if (a.length() > b.length())
+	itIs = a;
+    else 
+        itIs = b;
+    return itIs;
+
  }
 }
 
