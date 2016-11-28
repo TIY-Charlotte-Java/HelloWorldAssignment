aebdfe3de32e0cae12d1ea21248694a85b2e4cd8
Ben <bburrva@gmail.com>
2016-11-28 14:16:00 -0500

    Print my name in java file.

---
+++ HelloWorldAssignment.java
@@ -0,0 +1,25 @@
+class BenBurr {
+	public static void main(String[] args) {
+		System.out.println("Benjamin Burr");
+	}
+}
+
+/*
+Benjamin Burr
+println is a method
+create java file with echo "string" >filename.java
+edit file with textedit to format to java
+class "NameOfClass" {
+	public static void main(String[] args) {
+		System.out.println("scrypt");
+	}
+}
+basic format for java. 
+
+then:
+git add .
+git status - look for newly added java file
+git commit
+git status - should be 
+
+*/
