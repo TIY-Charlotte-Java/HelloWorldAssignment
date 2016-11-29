b03a63f3d807977db728cee7f5474e350f20d15e
Ser-Castic <samsteinberg90@gmail.com>
2016-11-28 19:27:14 -0500

    Created another method to that my name is contained in a variable - HARD mode inside RIDICULOUS mode

--- HelloMe.java
+++ HelloMe.java
@@ -1,5 +1,13 @@
-class HelloMe {
-	public static void main(String[] args) {
-		System.out.println("Samuel Steinberg");
-	}
+public class HelloMe
+{
+	public static void main(String[] args)
+	{
+		findName();
+	}	
+	public static void findName() 
+	{
+		String name;
+		name = new String("Samuel Steinberg");
+		System.out.println(name);	
+	}		
 }
